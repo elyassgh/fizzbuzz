bb4f40ccfc929ffb10bd34bdbc91e47f933ac433
elyassgh <elghazi.elyass@gmail.com>
2022-03-10 18:10:39 +0100

    reg-green-refactoring :traffic_ligh:.

--- src/main/java/FizzBuzzEngine.java
+++ src/main/java/FizzBuzzEngine.java
@@ -3,10 +3,8 @@ public class FizzBuzzEngine {
     public static String fizzbuzz(int n) throws Exception {
 
         if (n == 0) throw new Exception("Invalid Argument");
-
         StringBuilder buffer = new StringBuilder("");
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             buffer.append(FizzBuzzConverter.convert(i)).append(" ");
         }
 
--- src/test/java/ExpectedData.java
+++ src/test/java/ExpectedData.java
@@ -4,6 +4,6 @@ public class ExpectedData {
     static String fbFor2 = "1 2 ";
     static String fbFor3 = "1 2 Fizz ";
     static String fbFor5 = "1 2 Fizz 4 Buzz ";
-    static String fbFor15 = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz";
+    static String fbFor15 = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ";
 
 }
