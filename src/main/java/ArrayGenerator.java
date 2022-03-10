797def4f835b2296c6a26332fadb8a947e3bc4c0
elyassgh <elghazi.elyass@gmail.com>
2022-03-10 17:31:53 +0100

    obvious implentation of sequence numbers array generator

--- src/main/java/ArrayGenerator.java
+++ src/main/java/ArrayGenerator.java
@@ -1,15 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ArrayGenerator {
 
     static List<Integer> generate(int n) {
-
-        if (n == 0) return new ArrayList<>();
-
-        return Collections.singletonList(1);
+        return IntStream.rangeClosed(1, n).boxed().collect(Collectors.toList());
     }
 
 }
--- src/test/java/ArrayGeneratorTest.java
+++ src/test/java/ArrayGeneratorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,8 +9,11 @@ import static org.junit.Assert.assertEquals;
 
 public class ArrayGeneratorTest {
 
-    static List<Integer> EMPTY_LIST = new ArrayList<>();
-    static List<Integer> ONE_LIST = Collections.singletonList(1);
+    List<Integer> EMPTY_LIST = new ArrayList<>();
+    List<Integer> ONE_LIST = Collections.singletonList(1);
+    List<Integer> FIVE_LIST = Arrays.asList(1, 2, 3, 4, 5);
+    List<Integer> TEN_LIST = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
 
     @Test
     public void itShouldReturnEmptyListWhenZeroIsGiven() {
@@ -22,4 +26,17 @@ public class ArrayGeneratorTest {
         List<Integer> result = ArrayGenerator.generate(1);
         assertEquals(ONE_LIST, result);
     }
+
+    @Test
+    public void itShouldReturnListFrom1To5When5isGiven() {
+        List<Integer> result = ArrayGenerator.generate(5);
+        assertEquals(FIVE_LIST, result);
+    }
+
+    @Test
+    public void itShouldReturnListFrom1To10When10isGiven() {
+        List<Integer> result = ArrayGenerator.generate(10);
+        assertEquals(TEN_LIST, result);
+    }
+
 }
